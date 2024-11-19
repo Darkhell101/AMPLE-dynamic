169792adbf11bd03c4bdb8b330f0b6bf00ab5718
Darkhell <37462013+Darkhell101@users.noreply.github.com>
2024-11-19 09:51:14 +0800

    Update ample.m for implicit algorithm

--- ample.m
+++ ample.m
@@ -19,38 +19,49 @@
 % POSTPRO               - post processing function including vtk output
 %--------------------------------------------------------------------------
 clear;
-addpath('constitutive','functions','plotting','setup');        
-[lstps,g,mpData,mesh] = setupGrid;                                          % setup information
-NRitMax = 10; tol = 1e-9;                                                   % Newton Raphson parameters
-[nodes,nD] = size(mesh.coord);                                              % number of nodes and dimensions
-[nels,nen] = size(mesh.etpl);                                               % number of elements and nodes/element
-nDoF = nodes*nD;                                                            % total number of degrees of freedom
-nmp  = length(mpData);                                                      % number of material points
-lstp = 0;                                                                   % zero loadstep counter (for plotting function)
-uvw  = zeros(nDoF,1);                                                       % zeros displacements (for plotting function)
-run postPro;                                                                % plotting initial state & mesh
-for lstp=1:lstps                                                            % loadstep loop
-  fprintf(1,'\n%s %4i %s %4i\n','loadstep ',lstp,' of ',lstps);             % text output to screen (loadstep)
-  [mesh,mpData] = elemMPinfo(mesh,mpData);                                  % material point - element information
-  fext = detExtForce(nodes,nD,g,mpData);                                    % external force calculation (total)
-  fext = fext*lstp/lstps;                                                   % current external force value
-  oobf = fext;                                                              % initial out-of-balance force
-  fErr = 1;                                                                 % initial error
-  frct = zeros(nDoF,1);                                                     % zero the reaction forces
-  uvw  = zeros(nDoF,1);                                                     % zero the displacements
-  fd   = detFDoFs(mesh);                                                    % free degrees of freedom
-  NRit = 0;                                                                 % zero the iteration counter
-  Kt   = 0;                                                                 % zero global stiffness matrix
-  while (fErr > tol) && (NRit < NRitMax) || (NRit < 2)                      % global equilibrium loop
-    [duvw,drct] = linSolve(mesh.bc,Kt,oobf,NRit,fd);                        % linear solver
-    uvw  = uvw+duvw;                                                        % update displacements
-    frct = frct+drct;                                                       % update reaction forces
-    [fint,Kt,mpData] = detMPs(uvw,mpData);                                  % global stiffness & internal force
-    oobf = (fext-fint+frct);                                                % out-of-balance force
-    fErr = norm(oobf)/norm(fext+frct+eps);                                  % normalised oobf error
-    NRit = NRit+1;                                                          % increment the NR counter
-    fprintf(1,'%s %2i %s %8.3e\n','  iteration ',NRit,' NR error ',fErr);   % text output to screen (NR error)
-  end                           
-  mpData = updateMPs(uvw,mpData);                                           % update material points
-  run postPro;                                                              % Plotting and post processing 
+addpath('constitutive','functions','plotting','setup','ProgressBar','construct');
+tic;
+[lstps,dt,g,mpData,mesh] = setupGrid_collapse;                                  % setup information
+NRitMax = 10; tol = 1e-9;                                                       % Newton Raphson parameters
+[nodes,nD] = size(mesh.coord);                                                  % number of nodes and dimensions
+[nels,nen] = size(mesh.etpl);                                                   % number of elements and nodes/element
+nDoF = nodes*nD;                                                                % total number of degrees of freedom
+nmp  = length(mpData);                                                          % number of material points
+lstp = 0;                                                                       % zero loadstep counter (for plotting function)
+run postPro;                                                                    % plotting initial state & mesh
+
+for lstp=progress(1:lstps)
+    [mesh,mpData] = elemMPinfo(mesh,mpData);                                    % material point - element information
+    fext = detExtForce(nodes,nD,g,mpData);                                      % external force calculation (total)
+    [M,V0,A0] = detMVA(nodes,nD,g,mpData);                                      % mapping mass, velocity, acceleration to grid
+    oobf = fext;                                                                % initial out-of-balance force
+    fErr = 1;                                                                   % initial error
+    frct = zeros(nDoF,1);                                                       % zero the reaction forces
+    uvw  = zeros(nDoF,1);                                                       % zero the displacements
+    fd   = detFDoFs(mesh);                                                      % free degrees of freedom
+    NRit = 0;                                                                   % zero the iteration counter
+    Kt   = 0;                                                                   % zero global stiffness matrix
+    while (fErr > tol) && (NRit < NRitMax) || (NRit < 2)                        % global equilibrium loop
+        [duvw,drct] = linSolve(mesh.bc,Kt,oobf,NRit,fd);                        % linear solver
+        uvw  = uvw+duvw;                                                        % update displacements
+        frct = frct+drct;                                                       % update reaction forces
+        [fint,Kt,mpData] = detMPs(uvw,mpData,nD);                               % global stiffness & internal force
+        [Kd,Fdym] = detDym(uvw,M,V0,A0,dt,nD);                                  % I change your detDym function since your Fdym may be wrong (by Mian Xie)
+        Kt=Kt+Kd;
+        oobf = (fext-fint+frct-Fdym);                                           % out-of-balance force
+        fErr = norm(duvw)/norm(uvw+eps);                                        % normalised uvw error
+        NRit = NRit+1;                                                          % increment the NR counter
+    end
+    
+    % For a Newmark scheme, you have to update the grid velocity and 
+    % acceleration after you find a converged value (by Mian Xie)
+    V = 2*uvw/dt-V0;
+    A = 4*uvw/dt/dt-4*V0/dt-A0;
+    
+    mpData = updateMPs(uvw,A,dt,mpData);                                        % update material points
+    run postPro;                                                                % Plotting and post processing
 end
+
+elapsedTime=toc;
+fprintf('\nTotal time of load  step is %4.3fs\n', lstps*dt);
+fprintf('Total time of simulation is %4.3fs\n', elapsedTime);
